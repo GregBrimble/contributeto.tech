71798b213da343505f3729c36d81c6299e7bdbed
Greg Brimble <developer@gregbrimble.com>
2018-10-20 01:30:11 +0200

    GitHub v2

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,24 +1,28 @@
-from binascii import hexlify
-from os import urandom, getenv
+from os import getenv
 
 from flask import Flask, render_template, redirect, url_for
 from flask_dance.contrib.github import make_github_blueprint, github
 
-api = Flask(__name__)
+api = Flask(__name__, template_folder='../frontend')
 api.debug = True
-api.secret_key = hexlify(urandom(24))
+api.secret_key = getenv('SECRET_KEY')
 
 github_oauth_blueprint = make_github_blueprint(
     client_id=getenv('GITHUB_OAUTH_CLIENT_ID'),
     client_secret=getenv('GITHUB_OAUTH_CLIENT_ID'),
 )
-api.register_blueprint(github_oauth_blueprint, url_prefix="/login")
+api.register_blueprint(github_oauth_blueprint, url_prefix='/login')
+
+
+@api.route('/')
+def index():
+    if github.authorized:
+        return redirect(url_for('recommendations'))
+    return render_template('index.html')
 
 
 @api.route('/recommendations')
-def index():
-    if not github.authorized:
-        return redirect(url_for("google.login"))
-    resp = github.get("/user")
+def recommendations():
+    resp = github.get('/user')
     assert resp.ok
-    return "You are @{login} on GitHub".format(login=resp.json()["login"])
+    return 'You are @{login} on GitHub'.format(login=resp.json()['login'])
