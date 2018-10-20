3f3731124ee8337c973570cedc4353134b9fa356
Greg Brimble <developer@gregbrimble.com>
2018-10-20 14:48:11 +0200

    Resources working

--- backend/api.py
+++ backend/api.py
@@ -5,6 +5,25 @@ from flask_dance.contrib.github import github
 
 api = Blueprint('api', __name__)
 
+"""
+fragment interestedInDetails on Repository {
+  ...contributedToDetails
+  openIssues: issues(states:OPEN, first:5, orderBy:{field:CREATED_AT, direction:ASC}){
+    totalCount
+    edges{
+      node{
+        url
+        title
+        bodyHTML
+        reactions{
+          totalCount
+        }
+      }
+    }
+  }
+}
+"""
+
 
 def make_request(query):
     resp = github.post('/graphql', json={'query': query})
@@ -28,3 +47,53 @@ def me():
     resetAt
   }
 }"""))
+
+
+def get_contributed_to_repositories():
+    return make_request("""
+    fragment contributedToDetails on Repository {
+      name
+      forkCount
+      shortDescriptionHTML
+      languages(first:3, orderBy:{field:SIZE, direction: DESC}) {
+        edges {
+          node {
+            name
+            color
+          }
+        }
+      }
+      repositoryTopics(first:20){
+        edges{
+          node{
+            topic{
+              name
+              relatedTopics{
+                name
+              }
+              stargazers{
+                totalCount
+              }
+            }
+          }
+        }
+      }
+    }
+
+    query contributedToRepositories {
+      viewer{
+        repositoriesContributedTo(first:5, orderBy:{field:STARGAZERS,direction:DESC}){
+          edges{
+            node{
+              ...contributedToDetails
+            }
+          }
+        }
+      }
+    }
+    """)
+
+
+@api.route('/recommendations')
+def get_recommendations():
+    return jsonify(get_contributed_to_repositories())
